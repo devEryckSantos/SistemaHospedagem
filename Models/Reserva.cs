2d1c03ac57ebbcfaaab1d9fc4fc80925f65e0036
Eryck Santos <dev.erycksantos@gmail.com>
2025-05-17 18:13:39 -0300

    Concluído!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -13,22 +13,40 @@ public class Reserva
 
     public void CadastrarHospedes(List<Pessoa> hospedes)
     {
-
+        // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido      
+        if (hospedes.Count <= Suite.Capacidade)
+        {
+            Hospedes = hospedes;
+        }
+        else
+        {
+            // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+            throw new ArgumentException("Capacidade de hospedes ultrapassada!");
+        }
     }
 
     public void CadastrarSuite(Suite suite)
     {
-
+        Suite = suite;
     }
 
     public int ObterQuantidadeHospedes()
     {
-        return 0;
+        // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+        return Hospedes.Count;
     }
 
     public decimal CalcularValorDiaria()
     {
-        decimal valor = 0;
+        // TODO: Retorna o valor da diária
+        // Cálculo: DiasReservados X Suite.ValorDiaria
+        decimal valor = DiasReservados * Suite.ValorDiaria;
+
+        // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+        if (DiasReservados == 10)
+        {
+            valor = valor * (1 - 10m / 100m);
+        }        
 
         return valor;
     }
